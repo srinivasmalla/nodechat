f931c28b57fbe0643b2d665b5a9c0167baf114d5
srinivasmalla <srinivasmalla@live.com>
2018-11-08 16:14:23 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express'),
+	app=express(),
+	server = require('http').createServer(app),
+	io=require('socket.io').listen(server);
+
+
+server.listen(3000);
+
+
+app.get('/',function(req,res){
+res.sendFile(__dirname + '/index.html');
+});
+
+
+app.get('/receiver',function(req,res){
+res.sendFile(__dirname + '/receiver.html');
+});
+
+
+io.sockets.on('connection',function(socket){
+socket.on('send message',function(data){
+io.sockets.emit('new message',data);
+
+});
+
+});
